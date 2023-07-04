ed299cee3f40c6d76896efc20ac5852cb0aae0ed
dariofusco <fdario@hotmail.it>
2023-07-04 15:49:05 +0200

    stampa in console i numeri

---
+++ js/main.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 100; i++) {
+    console.log (i)
+    if ((i % 2) == 0) {
+        let i = "Fizz"
+        console.log (i)
+    } else if ((i % 5) == 0) {
+        let i = "Buzz"
+        console.log(i)
+    } if (((i % 2) == 0) && ((i % 5) == 0)) {
+        let i = "FizzBuzz"
+        console.log (i)
+    } 
+}
